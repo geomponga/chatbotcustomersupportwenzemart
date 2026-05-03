768b8d4f98d0a62b021b1e59a75d13f30046adc5
geomponga <gmponga@live.eicc.edu>
2026-05-03 18:29:23 -0500

    Add files via upload

--- faq_data.py
+++ faq_data.py
@@ -64,7 +64,7 @@ FAQS = [
         "help", "assist", "question"
     ],
     "fallback": "It sounds like you're starting a conversation. You can say things like: 'Hi, I need help.'",
-    "answer": "Hello! I'm Lisa your AI agent, Welcome to Wenzemart Store®. How can I assist you today?"
+    "answer": "Hello! I'm Lisa your chatbot, Welcome to Wenzemart Store®. How can I assist you today?"
 }
 
 
